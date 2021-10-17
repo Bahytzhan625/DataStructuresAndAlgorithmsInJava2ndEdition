8847e679a8c95e75d085ae5c2b47b94e3b2d8b33
Bahytzhan625 <fenix.bahytzhan@gmail.com>
2021-10-17 15:36:46 +0500

    Init

--- src/main/java/chapter8/Huffman.java
+++ src/main/java/chapter8/Huffman.java
@@ -1,4 +1,13 @@
 package chapter8;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+/**
+ *
+ */
 public class Huffman {
 }
