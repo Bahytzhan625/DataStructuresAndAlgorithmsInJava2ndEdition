51f97c182f313166450b712c95e907c8fca1dc92
Bahytzhan625 <fenix.bahytzhan@gmail.com>
2021-11-02 22:46:00 +0500

    Init

--- src/test/java/chapter11/TestHashChain.java
+++ src/test/java/chapter11/TestHashChain.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 import static chapter11.TestHashTableBase.HASH_TABLE_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Тестирование сущности {@link HashChain}
@@ -36,6 +37,61 @@ public class TestHashChain {
 
 	@Test
 	public void testHashFunction() {
-		assert
+		assertEquals(1, hashChain.hashFunction(new Link(1)));
+		assertEquals(1, hashChain.hashFunction(new Link(201)));
+		assertEquals(150, hashChain.hashFunction(new Link(150)));
+		assertEquals(0, hashChain.hashFunction(new Link(400)));
+	}
+
+	@Test
+	public void testInsertMethod() {
+		LinkItem inserttedItem = new Link(101);
+
+		hashChain.insert(insertedItem);
+
+		assertEquals(insertedItem, hashChain.find(insertedItem));
+	}
+
+	@Test
+	public void testDeleteMethod() {
+		LinkItem itemForDeleting = new Link(1000);
+		hashChain.insert(itemForDeleting);
+		LinkItem deletedItem = hashChain.delete(itemForDeleting);
+
+		assertNotNull(deletedItem);
+		assertEquals(itemForDeleting, deletedItem);
+
+		deletedItem = hashChain.delete(itemForDeleting);
+		assertNull(deletedItem);
+	}
+
+	@Test
+	public void testGetDisplayDataMethod() {
+		TestHashTableBase.checkGettingElementsNumber(hashChain, HASH_TABLE_SIZE / 2, additionalItems);
+	}
+
+	@Test
+	public void checkGettingElementsNumber() {
+		TestHashTableBase.checkGettingElementsNumber(hashChain, HASH_TABLE_SIZE / 2, additionalItems);
+	}
+
+	@Test
+	public void checkHashTableSize() {
+		assertEquals(HASH_TABLE_SIZE, hashChain.getHashTableSize());
+	}
+
+	@Test
+	public void testGettingLoadFactor() {
+		assertEquals((HASH_TABLE_SIZE / 2) / (float) HASH_TABLE_SIZE, hashChain.getLoadFactor());
+	}
+
+	@Test (expected = UnsupportedOperationException.class)
+	public void testGettingHashArray() {
+		hashChain.getHashArray();
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testRehash() {
+		hashChain.rehash();
 	}
 }
